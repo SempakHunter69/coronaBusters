b4e1d345066bc4bac31f53ecfc9eac3b0aa6d1de
SempakHunter69 <semvakhunter69@gmail.com>
2024-07-13 10:53:35 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import CoronaBusterScene from './scenes/CoronaBusterScene';
+import { Game as MainGame } from './scenes/Game';
+import { AUTO, Scale, Game } from 'phaser';
+
+//  Find out more information about the Game Config at:
+//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+const config = {
+    type: AUTO,
+    wwidth: 400, //----------->1. canvas width 
+    height: 620, //----------->2. canvas height
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 0 }, //--------> 3. gravitasi 0 agar pesawat tidak jatuh
+        },
+    },
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: [
+        CoronaBusterScene
+    ]
+};
+
+export default new Game(config);
